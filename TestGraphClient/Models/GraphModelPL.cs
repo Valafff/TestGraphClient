6ce2d75a997306c7ad40cd737ebaf5133f925ee3
Vetal <Vetal@GB550PRO>
2025-03-10 22:42:51 +0300

    Add project files.

---
+++ TestGraphClient/Models/GraphModelPL.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+using QuikGraph;
+
+namespace TestGraphClient.Models
+{
+    public class GraphModel
+    {
+        public BidirectionalGraph<Node, Edge> Graph { get; }
+
+        public GraphModel()
+        {
+            Graph = new BidirectionalGraph<Node, Edge>();
+        }
+    }
+
+    public class Edge : IEdge<Node>
+    {
+        public Node Source { get; }
+        public Node Target { get; }
+        
+
+        public Edge(Node source, Node target)
+        {
+            Source = source;
+            Target = target;
+        }
+    }
+
+    public class Node : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public List<Port> LeftPorts { get; set; }
+        public List<Port> RightPorts { get; set; }
+        private double _x;
+        private double _y;
+
+        public double X
+        {
+            get => _x;
+            set
+            {
+                _x = value;
+                OnPropertyChanged(nameof(X));
+            }
+        }
+
+        public double Y
+        {
+            get => _y;
+            set
+            {
+                _y = value;
+                OnPropertyChanged(nameof(Y));
+            }
+        }
+
+        public Node(string name, int leftPortCount, int rightPortCount)
+        {
+            Name = name;
+            LeftPorts = new List<Port>();
+            for (int i = 0; i < leftPortCount; i++)
+            {
+                LeftPorts.Add(new Port { Node = this, Index = i, Y = 10 + i * 20 });
+            }
+
+            RightPorts = new List<Port>();
+            for (int i = 0; i < rightPortCount; i++)
+            {
+                RightPorts.Add(new Port { Node = this, Index = i, Y = 10 + i * 20 });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Port
+    {
+        public Node Node { get; set; }
+        public int Index { get; set; }
+        public double Y { get; set; }
+    }
+
+}
---
+++ TestGraphClientLogic/TestGraphClientLogic.cs
@@ -0,0 +1,155 @@
+﻿using System.Text;
+using System.Text.Json;
+using TestGraphModel;
+
+namespace TestGraphClientLogic
+{
+    public class TestGraphClientLogic
+    {
+        private HttpClient client;
+        private string serverUrl;
+        private Graph graph;
+        
+
+        public TestGraphClientLogic(string _uri = "http://localhost:3000/api/graph")
+        {
+            serverUrl = _uri;
+            client = new HttpClient();
+            graph = new Graph();
+        }
+
+        //Проверка соединения
+        public async Task<bool> ConnectionTest()
+        {
+            HttpResponseMessage response = await client.GetAsync($"{serverUrl}/ping");
+            return response.IsSuccessStatusCode;
+        }
+
+        //Получение модели графа
+        public async Task<Graph> GetGraph()
+        {
+            HttpResponseMessage response = await client.GetAsync($"{serverUrl}/sendgraph");
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                GraphGetting(content, ref graph, out bool _error);
+                if (_error)  return null; 
+                return graph;
+            }
+            else
+                return null;
+        }
+
+        //Создание узла
+        public async Task<Graph> CreateNode(Node _newNode)
+        {
+            //Если ошибка - возвращается null
+            if (_newNode == null) return null;
+            if (string.IsNullOrEmpty(_newNode.NodeName)) return null; 
+
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase, // Используем camelCase для JSON
+                WriteIndented = true // Форматировать JSON (опционально) чтобы не было каши в консоли
+            };
+            string json = JsonSerializer.Serialize(_newNode, options);
+
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync($"{serverUrl}/createnode", content);
+            if (response.IsSuccessStatusCode)
+            {
+                GraphGetting(await response.Content.ReadAsStringAsync(), ref graph, out bool _error);
+                if (_error) return null;
+                return graph;
+            }
+            //Если ошибка - возвращается null если statuscode 200 - возвращается обновленный граф
+            return null;
+        }
+
+        //Редактирование узла
+        public async Task<Graph> EditNode(Node _newNode)
+        {
+            //Если ошибка - возвращается  null если statuscode 200 - возвращается обновленный граф
+            if (_newNode == null) return null;
+            if (string.IsNullOrEmpty(_newNode.NodeName)) return null; 
+            Node temp = graph.Nodes.FirstOrDefault(n => n.Id == _newNode.Id);
+            if (temp == null) return null;
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            string json = JsonSerializer.Serialize(_newNode, options);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync($"{serverUrl}/editnode", content);
+            if (response.IsSuccessStatusCode)
+            {
+                GraphGetting(await response.Content.ReadAsStringAsync(), ref graph, out bool _error);
+                if (_error) return null;
+                return graph;
+            }
+            return null;
+        }
+
+        //Создание/редактирование ребра.
+        public async Task<Graph> CreateOrEditEdge(Edge _edge)
+        {
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            string json = JsonSerializer.Serialize(_edge, options);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync($"{serverUrl}/edgeworks", content);
+            if (response.IsSuccessStatusCode)
+            {
+                GraphGetting(await response.Content.ReadAsStringAsync(), ref graph, out bool _error);
+                if (_error) return null;
+                return graph;
+            }
+            return null;
+        }
+
+        //Удаление ребра
+        public async Task<Graph> DeleteEdge(int _edgeId)
+        {
+            var response = await client.DeleteAsync($"{serverUrl}/deleteedge/{_edgeId}");
+            if (response.IsSuccessStatusCode)
+            {
+                GraphGetting(await response.Content.ReadAsStringAsync(), ref graph, out bool _error);
+                if (_error) return null;
+                return graph;
+            }
+            return null;
+        }
+     
+        void GraphGetting(string _content, ref Graph _graph, out bool _error)
+        {
+            Graph tempGraph = new Graph();
+            ////Для правильной сериализации и десериализации  PascalCase 
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            try
+            {
+                tempGraph = JsonSerializer.Deserialize<Graph>(_content, options);
+                _graph = tempGraph;
+                _error = false;
+            }
+            catch (JsonException ex)
+            {
+                _error = true;
+                throw;
+            }
+        }
+
+    }
+}
