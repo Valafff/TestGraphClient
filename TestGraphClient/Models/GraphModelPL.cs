17fe2731ff37b418cd8cbf06a4363744b641e10e
Vetal <Vetal@GB550PRO>
2025-03-12 05:10:11 +0300

    add PL model

--- TestGraphClient/Models/GraphModelPL.cs
+++ TestGraphClient/Models/GraphModelPL.cs
@@ -1,72 +1,69 @@
 ﻿using System.ComponentModel;
 using QuikGraph;
 
+
 namespace TestGraphClient.Models
 {
-    public class GraphModel
-    {
-        public BidirectionalGraph<Node, Edge> Graph { get; }
+    public class GraphPL : BidirectionalGraph<NodePL, EdgePL> { };
 
-        public GraphModel()
-        {
-            Graph = new BidirectionalGraph<Node, Edge>();
-        }
+    public class EdgePL(NodePL source, NodePL target) : IEdge<NodePL>
+    {
+        public int Id { get; set; }
+        public NodePL Source { get; set; } = source;
+        public NodePL Target { get; set; } = target;
+        public PortPL PortSource { get; set; }
+        public PortPL PortTarget { get; set; }
     }
 
-    public class Edge : IEdge<Node>
+    public class NodePL : BaseNotify
     {
-        public Node Source { get; }
-        public Node Target { get; }
-        
+        public int Id { get; set; }
+        public int PortsNumber { get; set; }
 
-        public Edge(Node source, Node target)
-        {
-            Source = source;
-            Target = target;
+        private string _nodeName;
+        public string NodeNamePL 
+        { get => _nodeName;
+          set => SetField(ref _nodeName, value);
         }
-    }
 
-    public class Node : INotifyPropertyChanged
-    {
-        public string Name { get; set; }
-        public List<Port> LeftPorts { get; set; }
-        public List<Port> RightPorts { get; set; }
+        public NodeDataPL SimpleDataPL { get; set; }
+        public List<PortPL> LeftPorts { get; set; }
+        public List<PortPL> RightPorts { get; set; }
+
         private double _x;
-        private double _y;
-
         public double X
         {
             get => _x;
-            set
-            {
-                _x = value;
-                OnPropertyChanged(nameof(X));
-            }
+            set => SetField(ref _x, value);
         }
 
+        private double _y;
         public double Y
         {
             get => _y;
-            set
-            {
-                _y = value;
-                OnPropertyChanged(nameof(Y));
-            }
+            set => SetField(ref _y, value);
         }
 
-        public Node(string name, int leftPortCount, int rightPortCount)
-        {
-            Name = name;
-            LeftPorts = new List<Port>();
-            for (int i = 0; i < leftPortCount; i++)
-            {
-                LeftPorts.Add(new Port { Node = this, Index = i, Y = 10 + i * 20 });
-            }
 
-            RightPorts = new List<Port>();
-            for (int i = 0; i < rightPortCount; i++)
+
+        public NodePL(string name, int PortCount, int start_Y = 10, int step_Y = 20)
+        {
+            NodeNamePL = name;
+            LeftPorts = new List<PortPL>();
+            RightPorts = new List<PortPL>();
+
+
+            for (int i = 0; i < PortCount; i++)
             {
-                RightPorts.Add(new Port { Node = this, Index = i, Y = 10 + i * 20 });
+                if (i%2 == 0)
+                {
+                    RightPorts.Add(new PortPL { NodeOwner = this, LocalId = i, Y = start_Y + i * step_Y });
+                }
+                else
+                {
+                    LeftPorts.Add(new PortPL { NodeOwner = this, LocalId = i, Y = start_Y + i * step_Y });
+                }
+
             }
         }
 
@@ -77,11 +74,33 @@ namespace TestGraphClient.Models
         }
     }
 
-    public class Port
+    public class PortPL
     {
-        public Node Node { get; set; }
-        public int Index { get; set; }
+        public int Id { get; set; }
+        public int LocalId { get; set; }
+        public int InputPortNumber { get; set; }
+        public NodePL NodeOwner { get; set; }
+        public string? InputNodeName { get; set; }
+        public bool IsLeftSidePort { get; set; }
+
+        public double X { get; set; }
         public double Y { get; set; }
+    }
+
+
+    public class NodeDataPL:BaseNotify
+    {
+        private string _someText;
+        public string SomeText
+        { get => _someText;
+          set => SetField(ref _someText, value);
+        }
+        private int _someValue;
+        public int SomeValue 
+        {
+          get => _someValue;
+          set => SetField(ref _someValue, value);
+        }
     }
 
 }
